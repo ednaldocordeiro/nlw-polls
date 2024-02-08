4085affe198a87a36c57c3893aa1a3836594a634
Ednaldo Cordeiro <ednaldocordeiro2017@gmail.com>
2024-02-08 16:57:28 -0300

    save votes

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -2,6 +2,7 @@ import z from 'zod';
 import { FastifyInstance } from 'fastify';
 import { randomUUID } from 'crypto';
 import { prisma } from '../../lib/prisma';
+import { redis } from '../../lib/redis';
 
 export async function voteOnPoll(app: FastifyInstance) {
   app.post('/polls/:pollId/votes', async (request, reply) => {
@@ -37,6 +38,8 @@ export async function voteOnPoll(app: FastifyInstance) {
             id: userPreviousVoteOnPoll.id,
           },
         });
+
+        await redis.zincrby(pollId, -1, userPreviousVoteOnPoll.id);
       } else if (userPreviousVoteOnPoll) {
         return reply
           .status(400)
@@ -62,6 +65,9 @@ export async function voteOnPoll(app: FastifyInstance) {
         pollOptionId,
       },
     });
+
+
+    await redis.zincrby(pollId, 1, pollOptionId)
 
     return reply.status(201).send();
   });
